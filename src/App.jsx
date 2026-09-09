9e9f3e2a025dfab518e4345ef17b6bbe4311caab
Joel Lindström <joel.tommy.lindstrom.syne25lin@edu.tucsweden.se>
2026-09-09 11:02:06 +0200

    ToggleSwitch gjord

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import './App.css'
 //import Header from './components/Header'
 //import List from './components/List'
 //import Footer from './components/Footer'
-import OnClick from './components/OnClick'
+//import OnClick from './components/OnClick'
+import ToggleSwitch from './components/ToggleSwitch'
 
 function App() {
   return (
     <>
-      <OnClick />
+      <ToggleSwitch />
     </>
   )
 }
