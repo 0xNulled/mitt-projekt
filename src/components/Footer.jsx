a83142d82a5738fd10faeb312377be4813ae7a75
Skola <skolacc@MacBook-Pro-som-tillhor-Lindstrom.local>
2026-09-09 10:30:12 +0200

    Initial commit: React-projekt med grundkomponenter

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <>
+            <h3>I forgor how to make footer :/</h3>
+        </>
+    )
+}
+
+export default Footer;
---
+++ src/components/List.jsx
@@ -0,0 +1,14 @@
+function List() {
+    return (
+        <>
+        <ul>
+            <li>här</li>
+            <li>är</li>
+            <li>en</li>
+            <li>lista</li>
+        </ul>
+        </>
+    )
+}
+
+export default List;
