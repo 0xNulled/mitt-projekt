fa9944e844add4c99da34157182658a88b69363a
Joel Lindström <joel.tommy.lindstrom.syne25lin@edu.tucsweden.se>
2026-09-09 10:52:10 +0200

    OnClick klar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import './App.css'
-import Header from './components/Header'
-import List from './components/List'
-import Footer from './components/Footer'
+//import Header from './components/Header'
+//import List from './components/List'
+//import Footer from './components/Footer'
+import OnClick from './components/OnClick'
 
 function App() {
   return (
     <>
-      <Header />
-      <List />
-      <Footer />
+      <OnClick />
     </>
   )
 }
