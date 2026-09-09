ee84be5fe19fcf73500b45391a83c6fac45a4063
Joel Lindström <joel.tommy.lindstrom.syne25lin@edu.tucsweden.se>
2026-09-09 11:11:12 +0200

    ProfileCard fixat

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import './App.css'
+import ProfileCard from './components/ProfileCard'
 //import Header from './components/Header'
 //import List from './components/List'
 //import Footer from './components/Footer'
 //import OnClick from './components/OnClick'
-import ToggleSwitch from './components/ToggleSwitch'
+//import ToggleSwitch from './components/ToggleSwitch'
 
 function App() {
   return (
     <>
-      <ToggleSwitch />
+      <ProfileCard
+        name="Sarah Svenson"
+        role="Person"
+        initials="SS"
+      />  
     </>
   )
 }
